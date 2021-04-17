fb815cf61863fbc0bacd454ee8271c013294ee36
Rajesh Moka <rajeshmoka@icloud.com>
2021-04-18 03:33:33 +0530

    modal added

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ const getMovies = (e) => {
   }
 }
 
+getMovies({target : { value : 'marvel' }});
+
 input.addEventListener('keyup', debounce(getMovies, 500));
 
 const showMovies = (movies) => {
@@ -37,7 +39,7 @@ const showMovies = (movies) => {
 const getMovieDetails = (e) => {
   const id = e.currentTarget.getAttribute('id');
   const modal = document.querySelector('.modal');
-  modal.style.display = 'block';
+  modal.style.display = '';
   const parent = modal.querySelector('.movieDetails');
   parent.innerHTML = '';
   const url = `https://www.omdbapi.com/?i=${id}&apikey=a5a3bcde`;
@@ -49,15 +51,16 @@ const getMovieDetails = (e) => {
 }
 
 const renderModalContent = (data) => {
-  const { Poster, Title, Year, Runtime, Released, Director, Genre, Language, BoxOffice, imdbRating } = data;
+  const { Poster, Title, Year, Runtime, Released, Director, Genre, Language, BoxOffice, imdbRating, Country, Awards, imdbVotes } = data;
   const parent = document.querySelector('.movieDetails');
   parent.appendChild(addCross())
   const image = document.createElement('img');
+  image.classList.add('imgBorder');
   if(Poster !== 'N/A') image.setAttribute('src', Poster);
   else image.setAttribute('alt', 'Image');
   const movieDiv = document.createElement('div');
-  movieDiv.appendChild(getDiv('Title', Title));
-  movieDiv.appendChild(getDiv('imdbRating', imdbRating));
+  movieDiv.appendChild(getTitle(Title));
+  movieDiv.appendChild(getDiv('IMDb Rating', imdbRating));
   movieDiv.appendChild(getDiv('Year', Year));
   movieDiv.appendChild(getDiv('Duration', Runtime));
   movieDiv.appendChild(getDiv('Released', Released));
@@ -65,8 +68,19 @@ const renderModalContent = (data) => {
   movieDiv.appendChild(getDiv('Genre', Genre));
   movieDiv.appendChild(getDiv('Language', Language));
   movieDiv.appendChild(getDiv('Box Office', BoxOffice));
+  movieDiv.appendChild(getDiv('Country', Country));
+  movieDiv.appendChild(getDiv('Awards', Awards));
+  movieDiv.appendChild(getDiv('IMDb Votes', imdbVotes));
   parent.appendChild(image);
   parent.appendChild(movieDiv);
+}
+
+const getTitle = (Title) => {
+  const heading = document.createElement('h1');
+  heading.textContent = Title;
+  heading.style.maxWidth = '350px'
+  heading.style.marginBottom = '16px';
+  return heading;
 }
 
 const addCross = () => {
@@ -83,9 +97,11 @@ const addCross = () => {
 const getDiv = (label, value) => {
   const div = document.createElement('div');
   div.classList.add('flex');
-  const title = document.createElement('div');
+  div.style.marginBottom = '4px';
+  const title = document.createElement('strong');
   title.innerText = label;
-  title.style.width = '100px'
+  title.style.width = '120px';
+  title.style.color = '#eaa917';
   const valueDiv = document.createElement('div');
   valueDiv.style.marginLeft = '4px';
   valueDiv.innerText = value || 'N/A';
@@ -100,12 +116,18 @@ const movieCard = (movie) => {
   movieDiv.setAttribute('id', imdbID);
   movieDiv.classList.add('movie_card');
   const image = document.createElement('img');
+  image.classList.add('imgBorder');
   if(Poster !== 'N/A') image.setAttribute('src', Poster);
   else image.setAttribute('alt', 'Image')
-  const title = document.createElement('div');
-  title.textContent = `${Title} - ${Year}`;
+  const title = document.createElement('strong');
+  title.style.display = 'block';
+  title.textContent = Title;
+  const year = document.createElement('div');
+  year.style.color = 'gray';
+  year.textContent = Year;
   movieDiv.appendChild(image);
   movieDiv.appendChild(title);
+  movieDiv.appendChild(year);
   movieDiv.onclick = getMovieDetails;
   return movieDiv;
 }
